fb67704fcbfd75b0dce6b9ec309a9e7c8fd227a4
Cory <coryf65@gmail.com>
2021-01-16 16:24:33 -0600

    figured out mid point to repeat at

--- Assets/Scripts/Background/RepeatBG.cs
+++ Assets/Scripts/Background/RepeatBG.cs
@@ -7,13 +7,14 @@ namespace Cory.Sidescroller.Background
     public class RepeatBG : MonoBehaviour
     {
 
-        private Vector3 startPosition; // the reset 
-        [SerializeField] private float offset = 56.5f;
+        private Vector3 startPosition; // the reset
+        [SerializeField] private float offset = 56.4f;
 
         // Start is called before the first frame update
         void Start()
         {
             startPosition = transform.position;
+            offset = GetComponent<BoxCollider>().size.x / 2;  // the midpoint
         }
 
         // Update is called once per frame
